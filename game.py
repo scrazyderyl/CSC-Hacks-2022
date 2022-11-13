e665af48d74712b747cd525b3204549c48feae41
Jason Ye <ye8jason@gmail.com>
2022-11-12 23:56:06 -0500

    Updated game.py

--- game.py
+++ game.py
@@ -48,10 +48,8 @@ class Game:
             self.dragTarget = None
 
 class Piece_List:
-    def __init__(self, pieces, xpos, ypos):
-        self.pieces = pieces
-        self.xpos = xpos
-        self.ypos = ypos
+    def __init__(self, pieces):
+        self.pieces = [getattr(globals, piece["shape"])(piece["edges"]) for piece in pieces]
     
     def add_piece(self, piece):
         self.pieces.append(piece)
@@ -70,17 +68,18 @@ class Piece_List:
         return None
 
 class Board:
-    def __init__(self, outline, xpos, ypos, cell_size):
-        self.outline = outline
+    def __init__(self, outline):        
+        self.outline = [OutlinePiece(piece["shape"], piece["grid_x"], piece["gridy"]) for piece in outline]
         self.groups = []
 
-        self.xpos = xpos
-        self.ypos = ypos
-        self.cell_size = cell_size
-
     def new_group(self, piece):
-        group = PieceGroup(piece)
+        group = PieceGroup()
+        group.add(piece)
         self.groups.append(group)
+        return group
+
+    def remove_group(self, group):
+        self.groups.remove(group)
     
     def get_clicked(self, position, mouse_state):
         for group in self.groups:
@@ -101,16 +100,16 @@ class Board:
         gridy = math.floor((position[1] - self.ypos) / self.cell_size)
         return (gridx, gridy)
 
+    def fits_grid(self, target, position):
+        pass
+
     def get_compatible(self, target, grid_pos):
         if type(target) == "PieceGroup":
             compatible = [group for group in self.groups if group.can_accept_group(target, grid_pos)]
         elif type(target) == "Piece":
             compatible = [group for group in self.groups if group.can_accept_piece(target, grid_pos)]
                 
-        if len(compatible) == 0:
-            return None
-        else:
-            return compatible
+        return compatible
 
     def is_empty_and_not_adjacent(self, target, grid_pos):
         if type(target) == "PieceGroup":
@@ -127,36 +126,39 @@ class Board:
     
     def drop(self, target, position):
         grid_pos = self.get_grid_position(position)
-        group = self.get_compatible(target, grid_pos)
 
-        if group != None:
-            group.add(target)
-            target.drop(grid_pos)
-        elif self.is_empty_and_not_adjcent(grid_pos):
-            self.new_group(target)
-            target.drop(grid_pos)
+        if not self.fits_grid(target, position):
+            target.return_pieces()
+            return
+
+        groups = self.get_compatible(target, grid_pos)
+
+        if len(groups) == 1:
+            pass
+        elif len(groups > 1):
+            pass
+        elif self.is_empty_and_not_adjcent(target, grid_pos):
+            pass
         else:
             target.return_pieces()
 
 class PieceGroup:
-    def __init__(self, piece):
-        self.pieces = [piece]
-        piece.set_group(self)
+    def __init__(self):
+        self.pieces = []
+
+    def __init__(self, group):
+        self.pieces = group.pieces
     
     def add(self, piece):
-        if type(piece) == "PieceGroup":
-            self.merge(piece)
-        elif type(piece) == "Piece":
-            self.pieces.append(piece)
-            piece.set_group(self)
+        self.pieces.append(piece)
+        piece.set_group(self)
     
-    def remove(self, piece):
-        self.pieces.remove(piece)
-
     def merge(self, group):
         for piece in group.pieces:
             self.add(piece)
-            self.set_group(self)
+            
+    def remove(self, piece):
+        self.pieces.remove(piece)
 
     def check_clicked(self, position):
         for piece in self.pieces:
@@ -183,25 +185,26 @@ class PieceGroup:
         for piece in self.pieces:
             piece.return_piece()
 
+class Edge:
+    def __init__(self, slot, recessed):
+        self.slot = slot
+        self.recessed = recessed
+
 class Piece:
-    def __init__(self, type, xpos, ypos, size):
-        self.type = type
+    def __init__(self, shape):
+        self.shape = shape
         self.group = None
 
-        self.gridx = None
-        self.gridy = None
-
-        self.xpos = xpos
-        self.ypos = ypos
-        self.size = size
+        self.grid_x = None
+        self.grid_y = None
 
     def set_pos(self, x, y):
         self.xpos = x
         self.ypos = y
 
-    def set_grid_pos(self, gridx, gridy):
-        self.gridx = gridx
-        self.gridy = gridy
+    def set_grid_pos(self, grid_x, grid_y):
+        self.grid_x = grid_x
+        self.grid_y = grid_y
 
     def set_group(self, group):
         self.group = group
@@ -222,20 +225,24 @@ class Piece:
     def return_piece(self):
         pass
 
-class Edge:
-    def __init__(self, slot, recessed):
-        self.slot = slot
-        self.recessed = recessed
+class OutlinePiece:
+    def __init__(self, shape, gridx, gridy):
+        self.shape = shape
+        self.grid_x = gridx
+        self.grid_y = gridy
 
 class Cube(Piece):
-    def __init__(self, xpos, ypos, size, edges):
-        super.__init__("cube", xpos, ypos, size)
-        self.rect = rect(xpos, ypos, size, size)
+    def __init__(self, edges):
+        super.__init__("cube")
 
-        self.top = edges[0]
-        self.right = edges[1]
-        self.bottom = edges[2]
-        self.left = edges[3]
+        top = None; right = None; bottom = None; left = None
+
+        for side in edges.keys():
+            edge = edges[side]
+            slot = edge["slot"]
+            recessed = edge["recessed"]
+
+            setattr(self, side, Edge(slot, recessed))
     
     def contains_point(self, position):
         return self.rect.contains(position)
