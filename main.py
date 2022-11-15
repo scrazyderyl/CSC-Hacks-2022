0c319f397b6b5989faf3220dae8657a7af191485
vaageeshadas <48497816+vaageeshadas@users.noreply.github.com>
2022-11-14 23:02:55 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ game = Game()
 while True:
     clock.tick(60)
 
+    #check if obj is on grid?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -28,6 +29,10 @@ while True:
                 mouse_pressed = True
                 position = pygame.mouse.get_pos()
                 game.on_mousedown(position)
+            if event.button == 1:
+                print("drag connected pieces")
+            elif event.button == 3:
+                print("drag single piece")
         elif event.type == pygame.MOUSEBUTTONUP:
             mouse_pressed = False
             position = pygame.mouse.get_pos()
@@ -36,5 +41,14 @@ while True:
             if mouse_pressed:
                 position = pygame.mouse.get_pos()
                 game.on_mousemove(position)
+                if event.rel[0] > 0:
+                    print("Drag object to the right")
+                elif event.rel[1] > 0:
+                    print("Drag object down")
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_z:
+                print("Zoom out")
+            elif event.key == pygame.K_i:
+                print("Zoom in")
 
     clock.tick_busy_loop()
